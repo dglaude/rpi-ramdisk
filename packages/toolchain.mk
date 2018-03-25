3442f29a7be2eb204f26be7f48c1b04ba0f7948a
David Glaude <dglaude@users.noreply.github.com>
2018-03-25 23:54:35 +0200

    Update toolchain.mk

--- packages/toolchain.mk
+++ packages/toolchain.mk
@@ -4,4 +4,4 @@ export SYSROOT := $(shell readlink -f ..)/sysroot
 STAGE := $(BASEDIR)/root
 TOOLCHAIN := $(shell readlink -f ..)/toolchain/bin/arm-linux-gnueabihf-
 
-ARCH_CFLAGS := -pipe -march=armv7-a -marm -mthumb-interwork -mfpu=neon-vfpv4 -mtune=cortex-a7 -mabi=aapcs-linux -mfloat-abi=hard
+ARCH_CFLAGS := -pipe -march=armv6 -marm -mthumb-interwork -mfpu=vfp -mtune=cortex-a7 -mabi=aapcs-linux -mfloat-abi=hard
